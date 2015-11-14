71da5989713f11864faa97ef6c42181e971e7ab8
xboy_lee <dolphin2525@163.com>
2015-11-14 23:25:43 +0800

    ...

--- CodeBase/Classes/Helper/CBNetworkHelper.m
+++ CodeBase/Classes/Helper/CBNetworkHelper.m
@@ -38,7 +38,7 @@
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     manager.requestSerializer = [AFHTTPRequestSerializer serializer];
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json", @"text/plain", @"text/html", nil];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/plain", @"text/html", nil];
     
     NSDictionary *finalParams = parameters;
     if(self.beforeBlock) {
@@ -58,8 +58,8 @@
 {
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     manager.requestSerializer = [AFHTTPRequestSerializer serializer];
-    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
-    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",@"text/json", @"text/plain", @"text/html", nil];
+    //manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    //manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/json", @"text/plain", @"text/html", nil];
     
     NSDictionary *finalParams = parameters;
     if(self.beforeBlock) {
